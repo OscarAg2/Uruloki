04ce26aded318510bd7cba3bc649d43555b69e48
Oscar Aguilar Soliz <oscaraguilarfut@gmail.com>
2023-08-12 16:18:46 -0400

    Acceso DB y estilo tienda terminado

--- index.py
+++ index.py
@@ -1,7 +1,21 @@
-from flask import  Flask, render_template
-from datetime import datetime
-from googletrans import Translator
+from flask import  Flask, render_template, request
+from flask_paginate import Pagination,get_page_args
+import pyodbc
+
 app = Flask(__name__)
+
+# MySQL Configuration
+db_config = {
+    'server': 'uruloki.cxwhbhuw13bl.us-east-2.rds.amazonaws.com',
+    'database': 'Uruloki',
+    'user': 'admin',
+    'password': 'minionswar2',
+    'port': 1433  # Specify the port here
+}
+
+RESULTS_PER_PAGE = 12 # Number of results per page
+
+app.template_folder = "Templates"
 
 @app.route('/')
 def home():
@@ -12,8 +26,43 @@ def torneos():
     return render_template('torneos.html')
 
 @app.route('/Tienda')
-def tienda():
-    return render_template('tienda.html')
+def tienda(): 
+    page = request.args.get('page', 1, type=int)
+
+    conn = pyodbc.connect(
+        f"DRIVER=ODBC Driver 17 for SQL Server;"
+        f"SERVER={db_config['server']};"
+        f"DATABASE={db_config['database']};"
+        f"UID={db_config['user']};"
+        f"PWD={db_config['password']};"
+        f"PORT={db_config['port']}"
+    )
+
+    cursor = conn.cursor()
+
+    query = """
+        SELECT *
+        FROM (
+            SELECT *, ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS row_num
+            FROM OscarPrueba
+        ) AS t
+        WHERE row_num BETWEEN ? AND ?
+    """
+    
+    start_row = (page - 1) * RESULTS_PER_PAGE + 1
+    end_row = start_row + RESULTS_PER_PAGE - 1
+    
+    cursor.execute(query, (start_row, end_row))
+    data = cursor.fetchall()
+
+    # Calculate total number of pages
+    cursor.execute("SELECT COUNT(*) FROM OscarPrueba")
+    total_rows = cursor.fetchone()[0]
+    total_pages = (total_rows + RESULTS_PER_PAGE - 1) // RESULTS_PER_PAGE
+    
+    conn.close()
+
+    return render_template('tienda.html', data=data, page=page,total_pages=total_pages)
 
 if __name__ == '__main__':
     app.run(debug=True)
