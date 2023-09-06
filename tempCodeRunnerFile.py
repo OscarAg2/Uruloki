bf741b16ea07a93b2f2efd75e18d6f02d68ecde5
Oscar Aguilar Soliz <oscaraguilarfut@gmail.com>
2023-09-05 20:32:40 -0400

    Actualizacion

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-    SELECT *
-        FROM (
-            SELECT *, ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS row_num
-            FROM OscarPrueba
-        ) AS t
-        WHERE row_num BETWEEN ? AND ?
